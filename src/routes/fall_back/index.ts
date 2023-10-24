c769164a62ac051be487b24ff6773dc754395ff0
rash <orashusedmund@gmail.com>
2023-10-24 10:53:25 +0100

    took of the log

--- src/routes/fall_back/index.ts
+++ src/routes/fall_back/index.ts
@@ -4,9 +4,8 @@ import { render_defaults } from "../../defaults";
 const router = express.Router();
 
 router.get("/*", (_, res: Response) => { // last route to catch all routes
-    console.log("req");
     const current_route = "/error";
     res.render("error", { ...render_defaults, current_route, header: "ERROR", status: 404, message: "PAGE NOT FOUND" });
 });
 
-export default router; //680213210
+export default router;
