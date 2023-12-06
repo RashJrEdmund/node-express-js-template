a37386a94a3e276c2789823fd74f1f23d4e7e061
rash <orashusedmund@gmail.com>
2023-12-06 23:54:03 +0100

    used my links directly in view

--- src/defaults/index.ts
+++ src/defaults/index.ts
@@ -1,7 +1,4 @@
 // NOTE! +=> removing this content without cleaning them from the following files will break your code.
-const MY_GITHUB_URL = process.env.MY_GITHUB_URL;
-
-const MY_TWITTER_URL = process.env.MY_TWITTER_URL;
 
 const nav_content = [ // data used for rendering the navbar
     {
@@ -15,9 +12,6 @@ const nav_content = [ // data used for rendering the navbar
 ];
 
 export const render_defaults = {
-    MY_GITHUB_URL,
-    MY_TWITTER_URL,
-
     nav_content,
     title: "My Node Template",
 };
